ef0614b6ce75cf294f9794f3e9b78b2aea443837
Kaledaii <mishant480@gmail.com>
2026-02-15 21:25:38 +0545

    quote added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from random import randint as r 
 
 def joke():
     url = "https://icanhazdadjoke.com/"
@@ -14,10 +15,30 @@ def joke():
     except Exception as e:
         print("Error fetching joke:", e)    
 
+def quote():
+    url="https://api.freeapi.app/api/v1/public/quotes/random"
+    try:
+        response=requests.get(url)
+        data=response.json()
+        range=r(0, len(data)-1)
+        author=data[range]['author']
+        quote=data[range]['content']
+        print(f"Quote: {quote}\nAuthor: {author}")
+        user=input("Click 1. to get another quote\nClick 2. to exit\n")
+        if user=='1':
+            quote()
+        else: print("Goodbye!")
+    except Exception as e:
+        print("Error fetching quote:", e)
+  
+
+
 def main():
-    user=input("Click 1. to get a joke\nClick 2. to exit\n")
+    user=input("Click 1. to get a joke\nClick 2. to get a quote\nClick 3. to exit\n")
     if user=='1':
         joke()
+    elif user=='2':
+        quote()
     else: print("Goodbye!")
 
 
